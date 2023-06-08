1b07d9e1dea4f7c5004f77d9dd48f775b831c4cd
Clément Beaurain <cbeaurain@corp.free.fr>
2023-06-08 15:20:10 +0200

    marche mais si la data est triee a lenvers ne fonctionne pas

--- train.py
+++ train.py
@@ -1,51 +1,54 @@
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
-LEARNING_RATE = 0.000001
-EPOCHS = 5
+LEARNING_RATE = 0.3
+EPOCHS = 30
 
-def displayPlot(headers,mileage, price):
+def displayPlot(headers,mileage, price, estimate):
 	plt.figure(figsize=(10,6))
 	plt.xlabel(headers[0])
 	plt.ylabel(headers[1])
-	plt.plot(mileage, price, 'ob', label = 'data')
+	plt.scatter(mileage, price, marker='+', label = 'data')
+	plt.plot(mileage, estimate, color='red', label = 'estimate')
 	plt.show()
 
 def readCsv(filePath):
-	try:
-		f = open(filePath, 'r')
-	except OSError:
-		print('cannot open', filePath)
-		sys.exit(0)
-	headers = f.readline().split(',')
-	f.close()
+	headers = np.loadtxt(filePath, delimiter=',', unpack=True, max_rows=1, dtype=str)
 	mileage,price= np.loadtxt(filePath, delimiter=',', unpack=True, skiprows=1, dtype=int)
 	return headers,mileage,price
+
+def estimatePrice(thetaCoef, thetaFix, mileage):
+	return thetaCoef * mileage + thetaFix
 
 def costFunction(thetaCoef, thetaFix, mileage, price, m_length):
 	cost = 0
 	for i in range(m_length):
-		f = thetaCoef * mileage[i] + thetaFix
+		f = estimatePrice(thetaCoef, thetaFix, mileage[i])
 		cost = cost + (f - price[i]) ** 2
-	total_cost = 1 / (2 * m_length) * cost
-	return total_cost
-
-def derivatedFix(thetaCoef, thetaFix, mileage, price, m_length):
-	sum = 0
-	for tempmileage, tempprice in zip(mileage, price):
-		sum = sum + estimatePrice(thetaCoef, thetaFix, tempmileage) - tempprice
-	return sum / m_length
+	return cost / (m_length * 2)
 
 def derivatedCoef(thetaCoef, thetaFix, mileage, price, m_length):
 	sum = 0
-	for tempmileage, tempprice in zip(mileage, price):
-		sum = sum + (estimatePrice(thetaCoef, thetaFix, tempmileage) - tempprice) * tempmileage
+	for mileage_i, price_i in zip(mileage, price):
+		est = estimatePrice(thetaCoef, thetaFix, mileage_i)
+		print('est1', est)
+		sum = sum + (estimatePrice(thetaCoef, thetaFix, mileage_i) - price_i) * mileage_i
+	print('sum1', sum)
+	print('m_length1', m_length)
 	return sum / m_length
 
-def estimatePrice(thetaCoef, thetaFix, mileage):
-	return thetaCoef * mileage + thetaFix
-      
+def derivatedFix(thetaCoef, thetaFix, mileage, price, m_length):
+	sum = 0
+	for mileage_i, price_i in zip(mileage, price):
+		est = estimatePrice(thetaCoef, thetaFix, mileage_i)
+		print('est2', est)
+		sum = sum + estimatePrice(thetaCoef, thetaFix, mileage_i) - price_i
+	print('sum2', sum)
+	print('m_length2', m_length)
+	return sum / m_length
+			
 def linearRegression(mileage, price):
 	m_length = mileage.shape[0]
 	thetaCoef = 0
@@ -54,16 +57,19 @@ def linearRegression(mileage, price):
 		cost = costFunction(thetaCoef, thetaFix, mileage, price, m_length)
 		d_coef = derivatedCoef(thetaCoef, thetaFix, mileage, price, m_length)
 		d_fix = derivatedFix(thetaCoef, thetaFix, mileage, price, m_length)
+		print('d_coef, d_fix', d_coef, d_fix)
 		thetaCoef = thetaCoef - LEARNING_RATE * d_coef
 		thetaFix = thetaFix - LEARNING_RATE * d_fix
-		print('cost, coef, fix', cost, thetaCoef, thetaFix)
+		print('cost, coef, fix', cost, thetaCoef, thetaFix, '\n')
+	return thetaCoef, thetaFix
 
 def train(filePath):
 	headers,mileage,price = readCsv(filePath)
-	# displayPlot(headers,mileage,price)
-	linearRegression(mileage,price)
+	thetaCoef,thetaFix = linearRegression(mileage,price)
+	displayPlot(headers,mileage,price, estimatePrice(thetaCoef, thetaFix, mileage))
 	
 if __name__ == '__main__':
 	if (len(sys.argv) <= 1):
 		sys.exit('Usage: python train.py <csv file>')
-	train(sys.argv[1])
+	# train(sys.argv[1])
+	train('data.csv')
