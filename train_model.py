33762c0a04d01badee0c205e2c2677eed362bae6
Clément Beaurain <cbeaurain@corp.free.fr>
2023-07-18 16:28:08 +0200

    a denormaliser

--- train_model.py
+++ train_model.py
@@ -4,15 +4,15 @@ import plotly.express as px
 import ml_tools as tools
 from tqdm import tqdm
 import argparse
+import sys
 
-LEARNING_RATE = 0.035
+LEARNING_RATE = 1
 EPOCHS = 5000
 
 
 def display_plot(headers, df, estimate):
-    fig = px.scatter(df, x=headers[0], y=headers[1], title='Price / Mileage')
-    fig.add_scatter(x=df[headers[0]], y=estimate,
-                    mode='lines', name='Regression line')
+    fig = px.scatter(df, x=headers[0], y=headers[1], title="Price / Mileage")
+    fig.add_scatter(x=df[headers[0]], y=estimate, mode="lines", name="Regression line")
     fig.show()
 
 
@@ -50,27 +50,32 @@ def compute_gradient(mileage, price, theta_slope, theta_intercept):
     return dj_dslope, dj_dintercept
 
 
-def linear_regression(mileage, price):
+def linear_regression(mileage, price, best_learning_rate):
     theta_slope = 0
     theta_intercept = 0
     cost_array = []
     p_history = []
-    for i in range(EPOCHS):
+    for _ in range(EPOCHS):
         dj_dslope, dj_dintercept = compute_gradient(
-            mileage, price, theta_slope, theta_intercept)
+            mileage, price, theta_slope, theta_intercept
+        )
         theta_slope = theta_slope - LEARNING_RATE * dj_dslope
         theta_intercept = theta_intercept - LEARNING_RATE * dj_dintercept
-        cost_array.append(cost_function(
-            theta_slope, theta_intercept, mileage, price, len(mileage)))
+        cost_array.append(
+            cost_function(theta_slope, theta_intercept, mileage, price, len(mileage))
+        )
         p_history.append([theta_slope, theta_intercept])
-        if (len(cost_array) > 1 and (cost_array[-2] < cost_array[-1] or cost_array[-2] - cost_array[-1] < 10e-9)):
+        if len(cost_array) > 1 and (
+            cost_array[-2] < cost_array[-1] or cost_array[-2] - cost_array[-1] < 10e-9
+        ):
+            if best_learning_rate == False:
+                print("Training stopped after {} epochs".format(len(cost_array)))
             break
     return theta_slope, theta_intercept, cost_array
 
 
 def plot_cost(cost_array):
-    fig = px.line(x=np.arange(len(cost_array)),
-                  y=cost_array, title='Cost function')
+    fig = px.line(x=np.arange(len(cost_array)), y=cost_array, title="Cost function")
     fig.show()
 
 
@@ -81,56 +86,66 @@ def save_theta(theta_slope, theta_intercept, np_km, np_price):
     price_max = tools.max_(np_price)
     theta_slope = (theta_slope * (price_max - price_min)) / (km_max - km_min)
     theta_intercept = theta_intercept * (price_max - price_min) + price_min
-    np.save('theta.npy', [theta_slope, theta_intercept])
+    np.save("theta.npy", [theta_slope, theta_intercept])
 
 
 def find_best_learning_rate(theta_slope, theta_intercept):
-    best_learning = 0.001
-    _, _, cost = linear_regression(theta_slope, theta_intercept)
+    global LEARNING_RATE
+    best_learning = LEARNING_RATE
+    _, _, cost = linear_regression(theta_slope, theta_intercept, True)
     best_cost = cost[-1]
-    for i in tqdm(np.arange(0.001, 1, 0.001)):
-        global LEARNING_RATE
+    for i in tqdm(np.arange(1, 2, 0.001)):
         LEARNING_RATE = i
-        _, _, cost = linear_regression(theta_slope, theta_intercept)
-        if (cost[-1] < best_cost):
+        _, _, cost = linear_regression(theta_slope, theta_intercept, True)
+        if cost[-1] < best_cost:
             best_cost = cost[-1]
             best_learning = i
     print("Best learning rate:", best_learning)
-    LEARNING_RATE = best_cost
+    LEARNING_RATE = best_learning
 
 
-def train_model(file_path, display, cost, learning_rate: bool):
+def train_model(file_path, display, cost, learning_rate: bool, accuracy: bool):
     headers, df = read_csv(file_path)
-    np_km = df['km'].to_numpy()
-    np_price = df['price'].to_numpy()
+    np_km = df["km"].to_numpy()
+    np_price = df["price"].to_numpy()
     norm_x = tools.normalize_array(np_km)
     norm_y = tools.normalize_array(np_price)
-    if (learning_rate == True):
+    if learning_rate == True:
         find_best_learning_rate(norm_x, norm_y)
     normalized_theta_slope, normalized_theta_intercept, cost_array = linear_regression(
-        norm_x, norm_y)
-    if (cost == True):
+        norm_x, norm_y, False
+    )
+    if cost == True:
         plot_cost(cost_array)
-    if (display == True):
-        norm_df = pd.DataFrame({'km': norm_x, 'price': norm_y})
-        display_plot(headers, norm_df, estimate_price(
-            normalized_theta_slope, normalized_theta_intercept, norm_x))
-    save_theta(normalized_theta_slope,
-               normalized_theta_intercept, np_km, np_price)
+    if display == True:
+        norm_df = pd.DataFrame({"km": norm_x, "price": norm_y})
+        display_plot(
+            headers,
+            norm_df,
+            estimate_price(normalized_theta_slope, normalized_theta_intercept, norm_x),
+        )
+    save_theta(normalized_theta_slope, normalized_theta_intercept, np_km, np_price)
+    if accuracy == True:
+        print(f"Accuracy: {((1 - cost_array[-1]) * 100):.4f}%")
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='Train a linear regression model')
-    parser.add_argument('file_path', metavar='file_path',
-                        type=str, help='csv file path')
-    parser.add_argument('-d', '--display',
-                        help='display plot', action='store_true')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Train a linear regression model")
     parser.add_argument(
-        '-c', '--cost', help='plot cost function', action='store_true')
-    parser.add_argument('-e', '--epochs', help='number of epochs')
-    parser.add_argument('-l', '--learning_rate',
-                        action='store_true', help='find best learning rate')
+        "file_path", metavar="file_path", type=str, help="csv file path"
+    )
+    parser.add_argument("-d", "--display", help="display plot", action="store_true")
+    parser.add_argument(
+        "-a",
+        "--accuracy",
+        help="calculates accuracy",
+        action="store_true",
+    )
+    parser.add_argument("-c", "--cost", help="plot cost function", action="store_true")
+    parser.add_argument("-e", "--epochs", help="number of epochs")
+    parser.add_argument(
+        "-l", "--learning_rate", action="store_true", help="find best learning rate"
+    )
     args = parser.parse_args()
     try:
         tools.is_valid_path(args.file_path)
@@ -140,4 +155,6 @@ if __name__ == '__main__':
             LEARNING_RATE = float(args.learning_rate)
     except Exception as e:
         sys.exit(e)
-    train_model(args.file_path, args.display, args.cost, args.learning_rate)
+    train_model(
+        args.file_path, args.display, args.cost, args.learning_rate, args.accuracy
+    )
